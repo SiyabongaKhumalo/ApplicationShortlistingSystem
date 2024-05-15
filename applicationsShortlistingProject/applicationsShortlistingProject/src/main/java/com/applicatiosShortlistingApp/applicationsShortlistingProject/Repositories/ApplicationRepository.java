3458c9cf56f0665b17fac56391eff218b694e4a8
siyabongashabangu <Sya.IShabangu@gmail.com>
2024-05-15 09:17:55 +0200

    exnteded japRepository for all classes in repository

--- applicationsShortlistingProject/applicationsShortlistingProject/src/main/java/com/applicatiosShortlistingApp/applicationsShortlistingProject/Repositories/ApplicationRepository.java
+++ applicationsShortlistingProject/applicationsShortlistingProject/src/main/java/com/applicatiosShortlistingApp/applicationsShortlistingProject/Repositories/ApplicationRepository.java
@@ -5,6 +5,6 @@ import com.applicatiosShortlistingApp.applicationsShortlistingProject.Entities.A
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@Repositoryg
 public interface ApplicationRepository extends JpaRepository<Application, Integer> {
 }
